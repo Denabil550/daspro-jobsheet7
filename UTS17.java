f359c19cd267d2c112dce7e409627948f1eff20c
Denabil550 <mihsannabil550@gmail.com>
2025-10-22 11:33:13 +0700

    UTS

---
+++ UTS17.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UTS17 {
+    public static void main(String[] args) {
+
+        system.out.println("Prestasi Mahasiswa");
+        System.out.print("Jumlah Prestasi lomba Mahasiswa");
+        System.out.println("lomba panjat tebing");
+        
+    }
+}
