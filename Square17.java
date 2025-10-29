4053471998450a7d4f0ae083c2989e0229f4d0a2
Denabil550 <mihsannabil550@gmail.com>
2025-10-29 13:56:41 +0700

    Jobsheet 8

---
+++ Square17.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Square17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<=N-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
